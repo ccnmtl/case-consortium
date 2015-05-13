30250b0c55f177acb2b29dcc94a2275f4928d1dd
James Davis <jedavis@columbia.edu>
2015-05-13 14:14:43 -0400

    using bash (for no real reason instead of python) to parse the yaml to a js object

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 runserver:
 	hugo server --watch --buildDrafts --verboseLog=true -v --baseUrl=""
+createjs:
+	bash scripts/create_json_from_mds.sh
 deploy:
 	rm -rf public/*
 	/usr/local/bin/hugo-0.13 -s . -b 'http://case-consortium.stage.ccnmtl.columbia.edu/' && rsync -avp --delete public/ selma.ccnmtl.columbia.edu:/var/www/case-consortium/
---
+++ static/js/test.js
@@ -0,0 +1,374 @@
+var cases = [
+{
+ "id":"1",
+ "title":"Settle or fight?  Far Eastern Economic Review  and Singapore",
+},
+{
+ "id":"106",
+ "title":"Words of the Dying: CHCH TV and Constable Garrett Styles",
+},
+{
+ "id":"108",
+ "title":"Truthiness: This American Life and the Monologist",
+},
+{
+ "id":"109",
+ "title":"¿Amigo o enemigo? Wikileaks y the Guardian ",
+},
+{
+ "id":"110",
+ "title":"Higher Truth or Just the Facts? Hell and Back Again",
+},
+{
+ "id":"111",
+ "title":"Can Investigative Journalism Pay?: InvestigateWest and the Nonprofit Model",
+},
+{
+ "id":"112",
+ "title":"Credible Voice: WHO-Beijing and the SARS Crisis",
+},
+{
+ "id":"113",
+ "title":"Evacuate or Stay? Northshore LIJ and Hurricane Sandy",
+},
+{
+ "id":"114",
+ "title":"The Elusive Tuberculosis Case: The CDC and Andrew Speaker",
+},
+{
+ "id":"115",
+ "title":"Reportear una Verdad Explosiva: Boston Globe y los Abusos Sexuales  en la Iglesia Católica",
+},
+{
+ "id":"116",
+ "title":"Cumulative or Discrete Numbers: How Should Bloomberg Measure the Bailout?",
+},
+{
+ "id":"117",
+ "title":"Land Grab or Responsible Development? Oil Palm Concessions in Liberia",
+},
+{
+ "id":"118",
+ "title":"AllNovaScotia.com: Success or Anomaly?",
+},
+{
+ "id":"119",
+ "title":"A Newsroom Divided: Kenya, the Election Crisis, and the Nation Media Group",
+},
+{
+ "id":"12",
+ "title":"Chemical Fallout: The Milwaukee Journal Sentinel and the BPA Story",
+},
+{
+ "id":"120",
+ "title":"Radio Buddu and the Uganda Anti-Homosexuality Bill",
+},
+{
+ "id":"121",
+ "title":"Public Service or Illegal Act? The Weekly Mail and Bugging Staal Burger",
+},
+{
+ "id":"122",
+ "title":"Spike or Suffer? Jamhuri and the Bribe Offer",
+},
+{
+ "id":"123",
+ "title":"Seeking Partners for Press Freedom Media Council of Tanzania and DEFIR",
+},
+{
+ "id":"124",
+ "title":"Côte Sud Initiative: Integrated Development in Haiti",
+},
+{
+ "id":"125",
+ "title":"Scientific Method in the Real World: Experience Corps and the Johns Hopkins Study",
+},
+{
+ "id":"126",
+ "title":"Fighting Blackmail: Khadija Ismayilova and Azerbaijan’s First Family",
+},
+{
+ "id":"127",
+ "title":"From Compstat to Gov 2.0 Big Data in New York City Management",
+},
+{
+ "id":"128",
+ "title":"Public-Private Partnerships for Green Space in NYC",
+},
+{
+ "id":"129",
+ "title":"Salvation or Mirage? The New York Times Paywall",
+},
+{
+ "id":"130",
+ "title":"Fighting for Equality in Education: Student Activism in Post-apartheid South Africa",
+},
+{
+ "id":"131",
+ "title":"Tortoise or Hare? Fundación Carvajal and Buenaventura",
+},
+{
+ "id":"132",
+ "title":"Chasing the Community Newspaper Rainbow: The Whiteville News Reporter and the Digital Age",
+},
+{
+ "id":"133",
+ "title":"The Islanders and the Scientists: Post-tsunami Aid in the Nicobars",
+},
+{
+ "id":"134",
+ "title":"Merging Two Worlds?: Reston Dental Arts and American Dental Partners",
+},
+{
+ "id":"135",
+ "title":"Publishing Pollution Data in China: Ma Jun and the Institute of Public and Environmental Affairs",
+},
+{
+ "id":"136",
+ "title":"Process and Politics: Chinalco and the Resettlement of Morococha",
+},
+{
+ "id":"14",
+ "title":"Reporting an Explosive Truth: The Boston Globe and Sexual Abuse in the Catholic Church ",
+},
+{
+ "id":"142",
+ "title":"Catalyze Without Offense: Daily Nation Living Magazine and the 'Laura' Story",
+},
+{
+ "id":"146",
+ "title":"Swaziland, HIV and Option B+: What Can We Afford?",
+},
+{
+ "id":"147",
+ "title":"Covert Activity: the Washington Post, Edward Snowden and the National Security Agency",
+},
+{
+ "id":"148",
+ "title":"Just Enough Alarm: GlobalPost and the Syrian Chemical Attack S",
+},
+{
+ "id":"15",
+ "title":"Charting a Course for Change: Transforming the Albany Times Union in a Wired World",
+},
+{
+ "id":"16",
+ "title":"From Concept to Story: Time Magazine and 'America at 300 Million'",
+},
+{
+ "id":"17",
+ "title":"The Bakersfield Californian and Blogging the Courtroom",
+},
+{
+ "id":"18",
+ "title":"New England Cable News and the Terry Glenn Story",
+},
+{
+ "id":"19",
+ "title":"Crowdsourcing: Promise or Hazard? Part A: Debate at the Fort Myers, FL, News-Press",
+},
+{
+ "id":"2",
+ "title":"Frontline's 'The Last Abortion Clinic': What's Fair in a Video World?",
+},
+{
+ "id":"20",
+ "title":"Crowdsourcing at the Fort Myers, FL, News-Press Part B: 'Help Us Investigate'",
+},
+{
+ "id":"21",
+ "title":"Integrity on Deadline: ABC News and the Duke Lacrosse Photographs",
+},
+{
+ "id":"22",
+ "title":"Reputations to Lose: BBC versus the Blair Government",
+},
+{
+ "id":"23",
+ "title":"After the Storms: The South Florida Sun-Sentinel Investigates FEMA",
+},
+{
+ "id":"24",
+ "title":"A Life on the Line: The Christian Science Monitor and Jill Carroll",
+},
+{
+ "id":"25",
+ "title":"Public Death, Private Life: Army Major Alan Rogers and the  Washington Post",
+},
+{
+ "id":"27",
+ "title":"Privacy and the Public Interest: Frederick, MD, News-Post and the Bruce Ivins Story",
+},
+{
+ "id":"28",
+ "title":"News or Rumor? Politico and the Edwards Affair ",
+},
+{
+ "id":"29",
+ "title":"Digital Deadline: The St. Louis Post-Dispatch and the Kirkwood Shooting",
+},
+{
+ "id":"30",
+ "title":"God and Darwin: The York Daily Record and the Intelligent Design Trial",
+},
+{
+ "id":"31",
+ "title":"Elusive Story (dead)",
+},
+{
+ "id":"33",
+ "title":"Terror or Accident? Newsday and the Crash of American Airlines Flight 587",
+},
+{
+ "id":"34",
+ "title":"Terror or Accident? Newsday and the Crash of American Airlines Flight 587 (Video)",
+},
+{
+ "id":"35",
+ "title":"Privacy and the Public Interest. Part B: The Case Against Ivins (DEAD)",
+},
+{
+ "id":"36",
+ "title":"But is it News? The New York Times and the International Freedom Center",
+},
+{
+ "id":"37",
+ "title":"A Matter of Opinion: The Oregonian Editorial Board and Sam Adams ",
+},
+{
+ "id":"38",
+ "title":"Risky Business: John Harris, Jim VandeHei, and Politico Part A",
+},
+{
+ "id":"39",
+ "title":"Risky Business: John Harris, Jim VandeHei, and Politico Part B: A Winning Model?",
+},
+{
+ "id":"40",
+ "title":"Elusive Story: The Chicago Tribune Examines 'No Child Left Behind'",
+},
+{
+ "id":"43",
+ "title":"When the story is us: Miami Herald, Nuevo Herald and Radio Martí",
+},
+{
+ "id":"49",
+ "title":"Reporters or cops? CONTACTO and the search for Paul Schaefer",
+},
+{
+ "id":"50",
+ "title":"¿Periodistas o policías? CONTACTO y la búsqueda de Paul Schaefer",
+},
+{
+ "id":"51",
+ "title":"Not for Profit? The Voice of San Diego Experiment",
+},
+{
+ "id":"53",
+ "title":"The Facebook Conundrum: The New Haven Independent and the Annie Le Murder",
+},
+{
+ "id":"54",
+ "title":"Voices Behind Bars: National Public Radio and Angola State Prison",
+},
+{
+ "id":"55",
+ "title":"Into the Breach: Should Student Journalists Save Local Political Reporting?",
+},
+{
+ "id":"56",
+ "title":"Legal Jeopardy: Clarín Group and the Kirchners",
+},
+{
+ "id":"57",
+ "title":"Conflicted: The New York Times and the Bias Question",
+},
+{
+ "id":"59",
+ "title":"Worth a Thousand Words: The Associated Press and Lance Corporal Joshua Bernard",
+},
+{
+ "id":"60",
+ "title":"Privacidad y el Interés Público: Frederick, MD, News-Post y la Historia de Bruce Ivins. Primera Parte.",
+},
+{
+ "id":"61",
+ "title":"Privacidad e Interés Público: La Evidencia Contra Ivins. Segunda Parte.",
+},
+{
+ "id":"62",
+ "title":"Una Historia Esquiva: El Chicago Tribune y la ley 'Que Ningún Niño Quede Atrás'",
+},
+{
+ "id":"66",
+ "title":"Caricatured: Le Monde and the Mohammed Cartoons",
+},
+{
+ "id":"67",
+ "title":"Buy It or Make It? The Charlotte Observer and the Associated Press",
+},
+{
+ "id":"68",
+ "title":"The Jury is Out: The Long Beach Post and Online Local News",
+},
+{
+ "id":"69",
+ "title":"Dissonance: The Cleveland Plain Dealer and its Classical Music Critic",
+},
+{
+ "id":"70",
+ "title":"Friend or foe? WikiLeaks and the Guardian",
+},
+{
+ "id":"83",
+ "title":"Rating Teachers: a Job for Journalists? The Los Angeles Times and Value-Added Analysis",
+},
+{
+ "id":"84",
+ "title":"Grow Your Own? Rice Self-sufficiency in Timor-Leste",
+},
+{
+ "id":"85",
+ "title":"A Woman's Place? Photojournalist Lynsey Addario in Libya",
+},
+{
+ "id":"86",
+ "title":"Personal Foul? Deadspin and an Iconic Athlete",
+},
+{
+ "id":"87",
+ "title":"When BEST Intentions Go Awry: Arsenic Mitigation in Bangladesh",
+},
+{
+ "id":"9",
+ "title":"New England Cable News and the Terry Glenn Story (Video)",
+},
+{
+ "id":"90",
+ "title":"Community Savings, or Community Threat? California Policy for Ill and Elderly Inmates",
+},
+{
+ "id":"91",
+ "title":"Voluntary or Regulated? The Trans Fat Campaign in New York City",
+},
+{
+ "id":"92",
+ "title":"Mission vs Safety: OCHA Somalia and the Baidoa Raid",
+},
+{
+ "author":"Sir Francis Olivier",
+ "date":"2015-05-08T12:16:31-04:00",
+ "id":"397",
+ "title":"In the beginning...",
+},
+{
+ "date":"2015-05-08T12:16:31-04:00",
+ "title":"test",
+},
+{
+ "author":"Sir Francis Olivier",
+ "date":"2015-05-11T12:09:00-04:00",
+ "id":"123",
+ "title":"A long road",
+},
+]
