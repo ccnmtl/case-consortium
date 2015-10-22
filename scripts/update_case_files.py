d2ef55672d21e341582d17c760d83660f22ed41b
Cassandra Dunlop <cdulop@columbia.com>
2015-10-22 13:00:23 -0400

    forgot call only takes list

--- scripts/update_case_files.py
+++ scripts/update_case_files.py
@@ -14,59 +14,6 @@ import fnmatch
 
 file_to_search = sys.argv[1]
 
-def represents_int(s):
-    try: 
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-def get_trail_number(somestring):
-    somestring = somestring.split('_')
-    sl = somestring[4]
-    sl = sl[4].split('.')
-    return sl[0]
-
-def is_homepage(string):
-    '''would be better as regex - page with case_id_(some_number).html
-    but I couldn't get that working'''
-    # if fnmatch.fnmatch(file, '^case_id_(\d+).html$'):
-    #     print file
-    newname = string[8:-5]
-    if represents_int(newname):
-        print newname
-    
-         
-def is_casefile(somestring):
-    '''only process names of case files - the rest is a waste of time'''
-    if somestring.endswith(".html") and somestring.startswith("case_id_"):
-        return True
-    else:
-        return False
-
-
-def replace_homepage(directory):
-    '''Still figuring out how to integrate this with the rest of the script'''
-    '''Attempt 1 - check if it is a case html file - if so save first case go
-    through the filenames - check the length of the name AND if the name has
-    a trailing number save that '''
-    file_name = os.getcwd()
-    print file_name
-    shortestname == ""
-    '''Should probably move this check to the outer search folders function'''
-    if file_name == "layout":
-        list = os.listdir(file_name)
-        print list
-        type(list)
-        shortest_file = list[0]
-        lowest_number = 0
-        for each in list:
-            '''We find a case html file with shorter name'''
-            print each
-            if len(shortest_file) > len(each):
-                shortest_file = each
-
-
 
 def search_folders(file_name):
     for path, subdirs, files in os.walk(file_name):
@@ -76,7 +23,7 @@ def search_folders(file_name):
             if "_pid_0" in f_name:
                 print "Removing file : " + str(f_name)
                 file_path = os.path.join(path, f_name)
-                call(["git rm", file_path])
+                call(["git", "rm", file_path])
 
     # go over directory second time - # of files
     # to update should be smaller
@@ -92,12 +39,5 @@ def search_folders(file_name):
                 for line in fileinput.input(file_path):
                     newfile.write(line.replace(old_string, new_string))
                 newfile.close()
-
-    for path, subdirs, files in os.walk(file_name):
-        '''need to account for homepage - has shortest name
-        and corresponding duplicate has the lowest ending id number'''
-        pass
-
-
 
 search_folders(file_to_search)
