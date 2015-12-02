3ff2fe7c779e4de91e45168a53b7c7b708dae502
coati-00 <coati.00@gmail.com>
2015-12-02 18:05:17 -0500

    small change

--- scripts/replace_flash.py
+++ scripts/replace_flash.py
@@ -27,5 +27,21 @@ replacement_one = ("""<iframe width="560" height="315" """
 replacement_two = ("""" frameborder="0" allowfullscreen></iframe>""")
 
 
+'''
+Code for decoding... urllib.unquote(string_to_decode).decode('utf8') 
+u'../../files/videos/221/TextCards.flv'
+
+>>> urllib.unquote(string_to_find).decode('utf8')
+u'<embed type="application/x-shockwave-flash" src="mediaplayer.swf?file=../../files/videos/221/TextCards.flv" width="420" height="286" style="undefined" id="movie_player_1" name="movie_player_1" bgcolor="#ffffff" quality="high" wmode="transparent" autoplay="false" flashvars="file=../../files/videos/221/TextCards.flv">'
+
+scan files... 
+look for pattern that looks like the flash tag...
+decode
+see what video name it matches... 
+put video name in iframe tag
+replace entire flash tag with new iframe...
+'''
+
+
 file_to_search = sys.argv[1]
 dest_file = sys.argv[2]
