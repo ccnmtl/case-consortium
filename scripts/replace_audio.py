b39cbb9d9b105ba73bf33315fd852f50b0e03318
coati-00 <coati.00@gmail.com>
2016-02-15 19:01:13 -0500

    latest audio update

--- scripts/replace_audio.py
+++ scripts/replace_audio.py
@@ -1,5 +1,5 @@
 '''
-  Find home page duplicates and delete them, then update links to the duplicate
+  replace current audio files with html5 audio
 '''
 import fileinput
 import os
@@ -7,58 +7,65 @@ import re
 import sys
 import urllib
 from bs4 import BeautifulSoup
+from bs4 import Comment
 from subprocess import call
 
 
-list_of_mp3s = 'mp3_list.txt'
-location_of_cases = sys.argv[1]
+file_to_search = sys.argv[1]
 
 
-mp3list = open(list_of_mp3s, 'r+')
+def has_audio_as_img(tag):
+    return tag.has_attr('src') and str(tag.get('src')).endswith(".mp3")
 
 
-def update_audio_tags(location_of_cases):
-    file_path = os.path.abspath(location_of_cases)
-    # print file_path
-    for line in mp3list.readlines():
-        fline = line.split('casestudy')
-        # case_path = line[0]
-        # print case_path
-        audio_path = '../..' + str(fline[1])
-        #print audio_path
-        # case_path = str(line[0]).split('caseconsortium')
-        # print case_path
-        '''for testing on one case - different path'''
-        sline = line.split('casestudies')
-        # print sline
-        sline = str(sline[1]).split('files')
-        # print sline
-        # html_pg_path = str(case_path[1]) + 'casestudy/www/layout/'
-        html_pg_path = str(sline[0]) + 'casestudy/www/layout/'
-        # print html_pg_path
-        # dir_path = location_of_cases + html_pg_path
-        # print dir_path
-        # print os.path.abspath()
-        dir_path = os.path.abspath('.') + '/110/'
-        #print dir_path
-        for path, subdirs, files in os.walk(dir_path):
-            # print "print inside loop"
-            # print files
-            files.sort()
-            for f_name in files:
-                if f_name.endswith(".html"):
-                    # print f_name
-                    file_path = os.path.join(path, f_name)
-                    html_doc = open(file_path, 'r+')
-                    # print html_doc
-                    soup = BeautifulSoup(html_doc, 'html.parser')
-                    for img_tag in soup.find_all('img'):
-                        # print(img_tag.get('src'))
-                        if str(img_tag.get('src')) == str(audio_path):
-                            print "this file " + f_name + " contains the audio file " + audio_path
-     
-update_audio_tags(location_of_cases)
 
+def search_folders(file_name):
+    for path, subdirs, files in os.walk(file_name):
+        files.sort()
+        for f_name in files:
+            if f_name.endswith(".html"):
+                file_path = os.path.join(path, f_name)
+                htmldoc = open(file_path, 'r+')
+                soup = BeautifulSoup(htmldoc, "html.parser")
+                for img_tag in soup.find_all(has_audio_as_img):
+                    '''save location of mp3 file'''
+                    mp3_loc = img_tag.get('src')
+                    mp3loc = mp3_loc.replace(' ', '&#32;')
+                    # print mp3loc
+                    paragraph = img_tag.find_parents("p")
+                    '''comment out original image audio tag'''
+                    audiotag = '<audio controls><source src="' + mp3loc + '" type="audio/mpeg"></audio>'
+                    new_comment = Comment(str(img_tag))
+                    new_comment = new_comment + audiotag
+                    # audiotag = BeautifulSoup(audiotag)
+                    # new_comment.extend(audiotag)
+                    img_tag.replace_with(new_comment)
+                    # parent = new_comment.find_parents("p")
+                    # parent.append()
+                    #print parent
+                    #print type(parent)
+                    #audiotag = '<audio controls><source src="' + mp3loc + '" type="audio/mpeg"></audio>'
+                    #audiotag = BeautifulSoup(audiotag)#, "html.parser")
+                    # paragraph.extend(audiotag)
+                    #img_tag.replace_with(new_comment + unicode(audiotag))
+                    #img_tag.replace_with(new_comment + str(audiotag))
+                    #paragraph = img_tag.find_parents("p")
+                    #paragraph.append(audiotag)
+                    # p = paragraph.find_parents("p")
+                    # print p
+                    # print type(paragraph)
+                    #print audiotag
+                    #print type(audiotag)
+                    #parent.append(audiotag)
+                    #print parent
+                    #print type(parent)
+                    #print paragraph
+                    #print type(paragraph)
+                    newdoc = soup.prettify(soup.original_encoding)
+                    htmldoc.close()
+                    with open(file_path,"wb") as file:
+                        file.write(newdoc)
 
+search_folders(file_to_search)
 
 
