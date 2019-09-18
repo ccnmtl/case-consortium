c51d10b135b4bb7da99a8b5eb02b5d517b1701c0
Nick Buonincontri <nbuonin@gmail.com>
2019-09-18 13:43:42 -0400

    JS format commit
    Replaces tabs for spaces, convert CRLF to LF

--- static/js/main.js
+++ static/js/main.js
@@ -1,83 +1,80 @@
 /*
-	Verti by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+    Verti by HTML5 UP
+    html5up.net | @n33co
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    */
 
 (function($) {
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)'
+    });
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)'
-	});
+    $(function() {
+        var	$window = $(window),
+            $body = $('body');
 
-	$(function() {
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-		var	$window = $(window),
-			$body = $('body');
+        $window.on('load', function() {
+            $body.removeClass('is-loading');
+        });
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Dropdowns.
+        $('#nav > ul').dropotron({
+            mode: 'fade',
+            noOpenerFade: true,
+            speed: 300
+        });
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Off-Canvas Navigation.
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				mode: 'fade',
-				noOpenerFade: true,
-				speed: 300
-			});
+        // Navigation Toggle.
+        $(
+            '<div id="navToggle">' +
+            '<a href="#navPanel" class="toggle"></a>' +
+            '</div>'
+        ).appendTo($body);
 
-		// Off-Canvas Navigation.
+        // Navigation Panel.
+        $(
+            '<div id="navPanel">' +
+            '<nav>' +
+            $('#nav').navList() +
+            '</nav>' +
+            '</div>'
+        )
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'left',
+                target: $body,
+                visibleClass: 'navPanel-visible'
+            });
 
-			// Navigation Toggle.
-				$(
-					'<div id="navToggle">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-					'</div>'
-				)
-					.appendTo($body);
+        // Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
+        if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
+            $('#navToggle, #navPanel, #page-wrapper')
+                .css('transition', 'none');
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
-
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#navToggle, #navPanel, #page-wrapper')
-						.css('transition', 'none');
-
-	});
+    });
 
 })(jQuery);
